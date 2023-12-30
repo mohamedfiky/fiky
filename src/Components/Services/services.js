f8ac76f6ad1b6b108247d252658ca18bcce3f821
Mohamed El-Fiky <fiky.link@gmail.com>
2023-12-30 22:29:29 +0200

    error test #1 => pass & some work on services component

--- src/Components/Services/services.js
+++ src/Components/Services/services.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import "./services.css";
 
 function Services() {
   return (
-    <div>services</div>
+    <div className="services">
+        <h2 className="special-heading">Services</h2>
+        <p>I can efficiently work on various types of projects,
+             such as HTML, CSS & JavaScript projects, WordPress projects,
+              as well as React.js projects.
+        </p>
+    </div>
   )
 }
 
