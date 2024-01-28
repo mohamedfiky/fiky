df1c6d867a1065212dc7c436a8a4a959b72bcbc9
Mohamed El-Fiky <fiky.link@gmail.com>
2024-01-29 00:08:59 +0200

    add the projects dynamicly

--- src/Components/Projects/projects.js
+++ src/Components/Projects/projects.js
@@ -1,53 +1,73 @@
-import React, {useEffect} from 'react';
+import React, {useState ,useEffect} from 'react';
+import projects_data from "./../../projects.json";
 import "./projects.css";
-import Active from "./active.png";
-import Jackson from "./jackson.png";
 
 function Projects() {
 
+  const [projects, setProjects] = useState([]);
+  
   useEffect(() => {
-
-    let filters_lis = document.querySelectorAll(".filters li");
-    let all_projects = document.querySelectorAll(".projects-row .project");
-    let projects_row = document.querySelector(".projects-row");
-
-    filters_lis.forEach(li =>{
-
-      li.addEventListener("click", ()=>{
-
-        filters_lis.forEach(l =>{
-          l.classList.remove("active");
-        });
-
-        li.classList.add("active");
-
-        projects_row.style.animation = "gallery_animation 1s";
-
-        setTimeout(()=>{
-          projects_row.style.animation = "none";
-        },1000);
-
-        all_projects.forEach(project =>{
-
-          if(!project.classList.contains(li.dataset.filter)){
-            setTimeout(()=>{
-              project.style.display = "none";
-            },500);
-          }
-
-          else{
-            setTimeout(()=>{
-              project.style.display = "block";
-            },500);
-          }
-
-        });
-
-      });
-      
-    });
+    
+    setProjects(projects_data);
 
   }, []);
+
+
+  ////////////////////////////
+
+  let filters_lis = document.querySelectorAll(".filters li");
+  let all_projects = document.querySelectorAll(".projects-row .project");
+  let projects_row = document.querySelector(".projects-row");
+
+  filters_lis.forEach(li =>{
+
+    li.addEventListener("click", ()=>{
+
+      filters_lis.forEach(l =>{
+        l.classList.remove("active");
+      });
+
+      li.classList.add("active");
+
+      projects_row.style.animation = "gallery_animation 1s";
+
+      setTimeout(()=>{
+        projects_row.style.animation = "none";
+      },1000);
+
+      all_projects.forEach(project =>{
+
+        if(!project.classList.contains(li.dataset.filter)){
+          setTimeout(()=>{
+            project.style.display = "none";
+          },500);
+        }
+
+        else{
+          setTimeout(()=>{
+            project.style.display = "block";
+          },500);
+        }
+
+      });
+
+    });
+    
+  });
+
+  const portfolio_items = projects.map(project =>{
+    return(
+      <div key={project.id} className={`project all ${project.filter}`}>
+          <div className="project-info">
+            <img src={project.imgUrl}  alt={project.name} />
+            <div className="project-links">
+              <a key={project.id} className="details" href={`#${project.id}`}>Details</a>
+              <a target="_blank" href={project.link}>Link</a>
+            </div>
+          </div>
+        </div>
+    );
+  });
 
   return (
     <div className="projects container">
@@ -66,73 +86,11 @@ function Projects() {
         </div>
         <div className="projects-row">
 
-          <div className="project all html">
-            <div className="project-info">
-              <img src="projects-imgs/halla-w-tasa.png" />
-              <div className="project-links">
-                <a href="#1">Details</a>
-                <a href="#2">Link</a>
-              </div>
-            </div>
-          </div>
-          <div className="project all react">
-            <div className="project-info">
-              <img src={Active} />
-              <div className="project-links">
-                <a href="#1"></a>
-                <a href="#2"></a>
-              </div>
-            </div>
-          </div>
-          <div className="project all wordpress">
-            <div className="project-info">
-              <img src={Jackson} />
-              <div className="project-links">
-                <a href="#1"></a>
-                <a href="#2"></a>
-              </div>
-            </div>
-          </div>
-          <div className="project all react">
-            <div className="project-info">
-              <img src={Active} />
-              <div className="project-links">
-                <a href="#1"></a>
-                <a href="#2"></a>
-              </div>
-            </div>
-          </div>
-          <div className="project all wordpress">
-            <div className="project-info">
-              <img src={Jackson} />
-              <div className="project-links">
-                <a href="#1"></a>
-                <a href="#2"></a>
-              </div>
-            </div>
-          </div>
-          <div className="project all html">
-            <div className="project-info">
-              <img src="projects-imgs/halla-w-tasa.png" />
-              <div className="project-links">
-                <a href="#1"></a>
-                <a href="#2"></a>
-              </div>
-            </div>
-          </div>
-          <div className="project all react">
-            <div className="project-info">
-              <img src={Active} />
-              <div className="project-links">
-                <a href="#1"></a>
-                <a href="#2"></a>
-              </div>
-            </div>
-          </div>
+          {portfolio_items}
 
         </div>
     </div>
   )
 }
 
-export default Projects
+export default Projects ;
