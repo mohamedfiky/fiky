fbdb49c9cc7697ac99867e96d0ca5f837292be3a
Mohamed El-Fiky <fiky.link@gmail.com>
2024-02-08 11:02:53 +0200

    finish project-details page : STATIC

--- src/Components/ProjectDetails/project-details.js
+++ src/Components/ProjectDetails/project-details.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams, NavLink } from 'react-router-dom';
 import projects_data from "./../../projects.json";
 import "./project-details.css";
+import { MdArrowForwardIos } from "react-icons/md";
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
@@ -90,6 +91,28 @@ return (
             <img src="../projects-imgs/fresco-3.png"></img>
           </SwiperSlide>
         </Swiper>
+      </div>
+      <div className="project-info">
+        <div className="description">
+          <h3>Fresco Restaurant</h3>
+          <h4>An Italian food restaurant website</h4>
+          <p>In this website I show my ability to work on WordPress projects using Elementor plugin. The content of Portfolio page is fetched from an external API using XMLHttpRequest. The Contact Us page form works properly using WPForms and WP Mail SMTP plugins. This website's design is cloned from another site (link provided in the footer).</p>
+        </div>
+        <div className="skills-links">
+          <ul className="project-skills">Skills: 
+            <li><i><MdArrowForwardIos /></i><span>WordPress</span></li>
+            <li><i><MdArrowForwardIos /></i><span>Elementor</span></li>
+            <li><i><MdArrowForwardIos /></i><span>Starter Templates</span></li>
+          </ul>
+          <div className="project-links">
+            <span className="live-preview">Live Preview: 
+              <a href="https://fresco-foods.000webhostapp.com" target="_blank">Fresco Restaurant</a>
+            </span>
+            <span className="code">Code: 
+              <a href="https://github.com/mohamedfiky/weather-react" target="_blank">GitHub</a>
+            </span>
+          </div>
+        </div>
       </div>
     </div>
     
