8915bcca155a723f07d2924f64562c9caceb3230
Mohamed El-Fiky <fiky.link@gmail.com>
2024-02-15 11:02:17 +0200

    adding Email-JS to the form #1

--- src/Form/form.js
+++ src/Form/form.js
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from "@emailjs/browser";
 import "./form.css";
 import { IoIosSend } from "react-icons/io";
 
 
 function Form() {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_vjwt0kr", "template_3fp47mz", form.current, {
+        publicKey: "ZozqOgfZ1_PiAHDdL",
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          e.target.reset();
+          document.querySelector(".error").style.display = "none";
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
   return (
     <div className="contact-form container">
         <h2 className="sub-pages-heading">
             Send me a message
         </h2>
-        <form className="form">
+        <form className="form" ref={form} onSubmit={sendEmail}>
             <input type="text" name="name" placeholder="Your Name" required></input>
             <input type="email" name="email" placeholder="Your E-mail" required></input>
             <textarea name="message" placeholder="Your Message" required></textarea>
             <button type="submit" className="btn">Send Message <i><IoIosSend /></i></button>
         </form>
+        <div className="success">
+            Message sent successfully
+        </div>
+        <div className="error">
+            Something went wrong
+        </div>
     </div>
   )
 }
